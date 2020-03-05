5607dfcd944849a2e705401e5f1d256d2e97dcad
Andrew Hyndman <ajhyndman@hotmail.com>
2020-03-04 23:47:48 -0800

    Support selecting other pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { calcStat, Stat, StatsTable } from '@smogon/calc';
-import React, { ChangeEvent, useState } from 'react';
-import { scaleLinear, ScaleLinear } from 'd3-scale';
 import { path } from 'd3-path';
+import { scaleLinear, ScaleLinear } from 'd3-scale';
 import { clamp, sum } from 'ramda';
-import { Input, ThemeProvider, FormControlLabel, Switch } from '@material-ui/core';
+import React, { ChangeEvent, useState } from 'react';
+
+import {
+  Container,
+  FormControlLabel,
+  Input,
+  Switch,
+  TextField,
+  ThemeProvider,
+} from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import createPalette from '@material-ui/core/styles/createPalette';
+import { Autocomplete } from '@material-ui/lab';
+import { calcStat, SPECIES, Stat, StatsTable } from '@smogon/calc';
+import { shortForm } from '@smogon/calc/dist/stats';
 
 type Stats = StatsTable<number>;
 type ModernStat = Exclude<Stat, 'spc'>;
@@ -28,15 +38,6 @@ const RADIUS = SIZE / 2;
 const INPUT_SIZE = 40;
 
 const MAX_EVS = 508;
-
-const PIKACHU_BASE_STATS: Stats = {
-  atk: 55,
-  def: 40,
-  spa: 50,
-  spd: 50,
-  spe: 90,
-  hp: 35,
-};
 
 const INITIAL_STATS: Stats = {
   atk: 0,
@@ -83,12 +84,19 @@ const dataFromStats = (stats: Stats, scale: ScaleLinear<number, number>) => {
   return [stats.hp, stats.atk, stats.def, stats.spe, stats.spd, stats.spa].map(scale);
 };
 
-const computeActualStat = (key: ModernStat, effortValue: number, individualValue: number = 31) => {
-  return calcStat(8, key, PIKACHU_BASE_STATS[key], individualValue, effortValue, 50);
+const computeActualStat = (
+  key: ModernStat,
+  baseStat: number,
+  effortValue: number,
+  individualValue: number = 31,
+) => {
+  return calcStat(8, key, baseStat, individualValue, effortValue, 50);
 };
 
 function App() {
   const [stats, setStats] = useState(INITIAL_STATS);
+  const [pokemon, setPokemon] = useState('Pikachu');
+  const [isDynamaxed, setIsDynamaxed] = useState(false);
 
   const handleStatChange = (key: Stat) => (event: ChangeEvent<HTMLInputElement>) => {
     const numericValue = parseInt(event.target.value || '0');
@@ -96,63 +104,90 @@ function App() {
     setStats(stats => ({ ...stats, [key]: newValue }));
   };
 
+  const selectedSpecies = SPECIES[8][pokemon];
+
   return (
     <ThemeProvider theme={THEME}>
-      <FormControlLabel control={<Switch />} label="Dynamax" labelPlacement="start" />
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: '100vh',
-        }}
-      >
+      <Container maxWidth="xs">
+        <Autocomplete
+          options={Object.keys(SPECIES[8])}
+          getOptionLabel={option => option}
+          value={pokemon}
+          onChange={(e: ChangeEvent<any>, value: any) => {
+            setPokemon(value);
+          }}
+          renderInput={params => <TextField {...params} label="Pokemon" variant="outlined" />}
+        />
+
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isDynamaxed}
+              onChange={(e: any, value: any) => setIsDynamaxed(value)}
+              color="primary"
+            />
+          }
+          label="Dynamax"
+          labelPlacement="start"
+        />
         <div
           style={{
-            position: 'relative',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            minHeight: '100vh',
           }}
         >
-          <svg width={SIZE} height={SIZE} viewBox={`0 0 ${SIZE} ${SIZE}`}>
-            <g transform={`translate(${RADIUS} ${RADIUS})`}>
-              <path
-                d={drawHexagon([RADIUS, RADIUS, RADIUS, RADIUS, RADIUS, RADIUS])}
-                fill="white"
-              />
-              <path
-                d={drawHexagon(dataFromStats(stats, ev))}
-                fill={sum(Object.values(stats)) <= MAX_EVS ? 'gold' : 'red'}
-              />
-            </g>
-          </svg>
-
-          {(['hp', 'atk', 'def', 'spe', 'spd', 'spa'] as ModernStat[]).map((key, i) => {
-            const [x, y] = polarToCartesian([RADIUS + INPUT_SIZE, 2 * Math.PI * (i / 6)]);
-
-            return (
-              <div
-                style={{
-                  color: 'white',
-                  position: 'absolute',
-                  transform: `translate(${x}px, ${y}px)`,
-                  textAlign: 'center',
-                }}
-              >
-                <p style={{ margin: 0 }}>{STAT_LABEL[key]}</p>
-                <Input
-                  type="number"
-                  onChange={handleStatChange(key)}
-                  style={{ maxWidth: INPUT_SIZE }}
-                  value={stats[key]}
+          <div
+            style={{
+              position: 'relative',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <svg width={SIZE} height={SIZE} viewBox={`0 0 ${SIZE} ${SIZE}`}>
+              <g transform={`translate(${RADIUS} ${RADIUS})`}>
+                <path
+                  d={drawHexagon([RADIUS, RADIUS, RADIUS, RADIUS, RADIUS, RADIUS])}
+                  fill="white"
                 />
-                <p style={{ margin: '4px 0 0' }}>{computeActualStat(key, stats[key])}</p>
-              </div>
-            );
-          })}
+                <path
+                  d={drawHexagon(dataFromStats(stats, ev))}
+                  fill={sum(Object.values(stats)) <= MAX_EVS ? 'gold' : 'red'}
+                />
+              </g>
+            </svg>
+
+            {(['hp', 'atk', 'def', 'spe', 'spd', 'spa'] as ModernStat[]).map((key, i) => {
+              const [x, y] = polarToCartesian([RADIUS + INPUT_SIZE, 2 * Math.PI * (i / 6)]);
+
+              return (
+                <div
+                  style={{
+                    color: 'white',
+                    position: 'absolute',
+                    transform: `translate(${x}px, ${y}px)`,
+                    textAlign: 'center',
+                  }}
+                >
+                  <p style={{ margin: 0 }}>{STAT_LABEL[key]}</p>
+                  <Input
+                    type="number"
+                    onChange={handleStatChange(key)}
+                    style={{ maxWidth: INPUT_SIZE }}
+                    value={stats[key]}
+                  />
+                  <p style={{ margin: '4px 0 0' }}>
+                    {computeActualStat(key, selectedSpecies.bs[shortForm(key)]!, stats[key]) *
+                      (isDynamaxed && key === 'hp' ? 2 : 1)}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </Container>
     </ThemeProvider>
   );
 }
