a481f3766a9198660d037d3ed86aece9cd62e0c0
Andrew Hyndman <ajhyndman@hotmail.com>
2020-03-10 20:16:41 -0700

    fixup! fixup! Create components/ directory

--- src/components/TypeIcon.tsx
+++ src/components/TypeIcon.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 import { Type } from '@smogon/calc/dist/data/types';
 
-import iconBug from './assets/Bug_icon_SwSh.png';
-import iconDark from './assets/Dark_icon_SwSh.png';
-import iconDragon from './assets/Dragon_icon_SwSh.png';
-import iconElectric from './assets/Electric_icon_SwSh.png';
-import iconFairy from './assets/Fairy_icon_SwSh.png';
-import iconFighting from './assets/Fighting_icon_SwSh.png';
-import iconFire from './assets/Fire_icon_SwSh.png';
-import iconFlying from './assets/Flying_icon_SwSh.png';
-import iconGhost from './assets/Ghost_icon_SwSh.png';
-import iconGrass from './assets/Grass_icon_SwSh.png';
-import iconGround from './assets/Ground_icon_SwSh.png';
-import iconIce from './assets/Ice_icon_SwSh.png';
-import iconNormal from './assets/Normal_icon_SwSh.png';
-import iconPoison from './assets/Poison_icon_SwSh.png';
-import iconPsychic from './assets/Psychic_icon_SwSh.png';
-import iconRock from './assets/Rock_icon_SwSh.png';
-import iconSteel from './assets/Steel_icon_SwSh.png';
-import iconWater from './assets/Water_icon_SwSh.png';
+import iconBug from '../assets/Bug_icon_SwSh.png';
+import iconDark from '../assets/Dark_icon_SwSh.png';
+import iconDragon from '../assets/Dragon_icon_SwSh.png';
+import iconElectric from '../assets/Electric_icon_SwSh.png';
+import iconFairy from '../assets/Fairy_icon_SwSh.png';
+import iconFighting from '../assets/Fighting_icon_SwSh.png';
+import iconFire from '../assets/Fire_icon_SwSh.png';
+import iconFlying from '../assets/Flying_icon_SwSh.png';
+import iconGhost from '../assets/Ghost_icon_SwSh.png';
+import iconGrass from '../assets/Grass_icon_SwSh.png';
+import iconGround from '../assets/Ground_icon_SwSh.png';
+import iconIce from '../assets/Ice_icon_SwSh.png';
+import iconNormal from '../assets/Normal_icon_SwSh.png';
+import iconPoison from '../assets/Poison_icon_SwSh.png';
+import iconPsychic from '../assets/Psychic_icon_SwSh.png';
+import iconRock from '../assets/Rock_icon_SwSh.png';
+import iconSteel from '../assets/Steel_icon_SwSh.png';
+import iconWater from '../assets/Water_icon_SwSh.png';
 
 type Props = {
   type: Type;
