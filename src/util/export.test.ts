4113a8243a8e6d394a22ec3dd9bda1957546feac
Andrew Hyndman <ajhyndman@hotmail.com>
2020-03-13 22:22:55 -0700

    Export Pokemon class-supported features to text

---
+++ src/util/export.test.ts
@@ -0,0 +1,53 @@
+import { Pokemon } from '@smogon/calc';
+
+import { exportPokemon } from './export';
+import { GENERATION } from './misc';
+
+describe('exportPokemon', () => {
+  it('Prints default pokemon', () => {
+    const text = exportPokemon(new Pokemon(GENERATION, 'Pikachu'));
+    expect(text).toBe(`Pikachu (M)
+Ability: Lightning Rod
+Level: 100
+Serious Nature`);
+  });
+
+  it('Prints gender', () => {
+    const text = exportPokemon(new Pokemon(GENERATION, 'Pikachu', { gender: 'male' }));
+    expect(text).toBe(`Pikachu (M)
+Ability: Lightning Rod
+Level: 100
+Serious Nature`);
+  });
+
+  it('Prints stats', () => {
+    const text = exportPokemon(
+      new Pokemon(GENERATION, 'Pikachu', {
+        ivs: { hp: 10, atk: 10, def: 10, spa: 10, spd: 10, spe: 10 },
+        evs: { hp: 20, atk: 20, def: 20, spa: 20, spd: 20, spe: 20 },
+      }),
+    );
+    expect(text).toBe(`Pikachu (M)
+Ability: Lightning Rod
+Level: 100
+IVs: 10 HP / 10 Atk / 10 Def / 10 Spe / 10 SpA / 10 SpD
+EVs: 20 HP / 20 Atk / 20 Def / 20 Spe / 20 SpA / 20 SpD
+Serious Nature`);
+  });
+
+  it('Prints pokemon with moves', () => {
+    const text = exportPokemon(
+      new Pokemon(GENERATION, 'Pikachu', {
+        moves: ['Fake Out', 'Thunder Wave', 'Thunderbolt', 'Volt Switch'],
+      }),
+    );
+    expect(text).toBe(`Pikachu (M)
+Ability: Lightning Rod
+Level: 100
+Serious Nature
+- Fake Out
+- Thunder Wave
+- Thunderbolt
+- Volt Switch`);
+  });
+});
---
+++ src/util/export.ts
@@ -0,0 +1,103 @@
+/**
+ * Export to the standard Pokemon text format.
+ *
+ * Smogonbirb (Talonflame) (F) @ Flyinium Z
+ * Ability: Gale Wings
+ * EVs: 252 Atk / 4 Def / 252 Spe
+ * Jolly Nature
+ * - Brave Bird
+ * - Flare Blitz
+ * - Swords Dance
+ * - Roost
+ *
+ * Full syntax description can be found here:
+ * https://pokepast.es/syntax.html
+ *
+ */
+import { Pokemon, Stat, StatsTable } from '@smogon/calc';
+
+const DEFAULT_IV = 31;
+const DEFAULT_EV = 0;
+
+const STAT_PRINT_NAMES: { [key in Stat]: string } = {
+  hp: 'HP',
+  atk: 'Atk',
+  def: 'Def',
+  spa: 'SpA',
+  spd: 'SpD',
+  spe: 'Spe',
+  spc: 'Spc',
+};
+
+const GENDER_PRINT_TEXT = {
+  female: ' (F)',
+  male: ' (M)',
+  genderless: '',
+};
+
+function statsAllDefault(defaultValue: number, stats: StatsTable): boolean {
+  return Object.values(stats).some(value => value === defaultValue);
+}
+
+function printStats(defaultValue: number, stats: StatsTable): string {
+  // @ts-ignore: TypeScript can't follow the type of key/value through map.
+  const ivEntries = Object.entries(stats).map(([key, value]: [Stat, number]) => {
+    if (value !== defaultValue) {
+      return `${value} ${STAT_PRINT_NAMES[key]}`;
+    }
+  }, '');
+  return ivEntries.join(' / ');
+}
+
+export function exportPokemon(pokemon: Pokemon): string {
+  const lines = [];
+
+  // Print first line
+  let firstLine = '';
+  firstLine += pokemon.name;
+  if (pokemon.gender && GENDER_PRINT_TEXT.hasOwnProperty(pokemon.gender)) {
+    firstLine += GENDER_PRINT_TEXT[pokemon.gender];
+  }
+  if (pokemon.item != null) {
+    firstLine += ` @ ${pokemon.item}`;
+  }
+  lines.push(firstLine);
+
+  // Print ability
+  if (pokemon.ability) {
+    const abilityLine = `Ability: ${pokemon.ability}`;
+    lines.push(abilityLine);
+  }
+
+  // Print level
+  if (pokemon.level) {
+    const levelLine = `Level: ${pokemon.level}`;
+    lines.push(levelLine);
+  }
+
+  // Print IVs
+  if (!statsAllDefault(DEFAULT_IV, pokemon.ivs)) {
+    const ivLine = `IVs: ${printStats(DEFAULT_IV, pokemon.ivs)}`;
+    lines.push(ivLine);
+  }
+
+  // Print EVs
+  if (!statsAllDefault(DEFAULT_EV, pokemon.evs)) {
+    const evLine = `EVs: ${printStats(DEFAULT_EV, pokemon.evs)}`;
+    lines.push(evLine);
+  }
+
+  // Print nature
+  if (pokemon.nature) {
+    const natureLine = `${pokemon.nature} Nature`;
+    lines.push(natureLine);
+  }
+
+  // Print all learned moves
+  pokemon.moves.forEach(move => {
+    const moveLine = `- ${move}`;
+    lines.push(moveLine);
+  });
+
+  return lines.join('\n');
+}
