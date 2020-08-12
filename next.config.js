a1eaac08b3885ffc086960bd15d837d477702668
Andrew Hyndman <ajhyndman@hotmail.com>
2020-08-11 21:39:38 -0700

    Add build:analyze script

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
   webpack: (config, { webpack, isServer }) => {
     // Inject custom polyfills
     const originalEntry = config.entry;
@@ -26,4 +30,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
